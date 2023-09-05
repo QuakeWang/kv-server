672f248131c11d24d11bddd04b3b83bf0e00c4e3
QuakeWang <1677980708@qq.com>
2023-09-05 18:15:46 +0800

    memory mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+mod error;
 mod pb;
+mod storage;
+
+pub use error::KvError;
 pub use pb::abi::*;
+pub use storage::*;
---
+++ src/storage/mod.rs
@@ -0,0 +1,101 @@
+mod memory;
+pub use memory::MemTable;
+use crate::{KvError, Kvpair, Value};
+
+/// This is abstrct of storage. 
+pub trait Storage {
+    /// Get a value of the key from HashTable
+    fn get(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
+    /// Update a value of the key from HashTable, and return the old value
+    fn set(&self, table: &str, key: String, value: Value) -> Result<Option<Value>, KvError>;
+    /// Check if the key contains in the HashTable
+    fn contains(&self, table: &str, key: &str) -> Result<bool, KvError>;
+    /// Delete a key from HashTable
+    fn del(&self, table: &str, key: &str) -> Result<Option<Value>, KvError>;
+    /// Return all kv pairs from HashTable (bad tarit)
+    fn get_all(&self, table: &str) -> Result<Vec<Kvpair>, KvError>;
+    /// Return kv pairs' Iterator of HashTable
+    fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn memtable_basic_interface_should_work() {
+        let store = MemTable::new();
+        test_basi_interface(store);
+    }
+
+    #[test]
+    fn memtable_get_all_should_work() {
+        let store = MemTable::new();
+        test_get_all(store);
+    }
+
+    // #[test]
+    // fn memtable_iter_should_work() {
+    //     let store = MemTable::new();
+    //     test_get_iter(store);
+    // }
+
+    fn test_basi_interface(store: impl Storage) {
+        // The first set creates the table, inserts the key and returns None.
+        let v = store.set("t1", "hello".into(), "world".into());
+        assert!(v.unwrap().is_none());
+        // Setting the same key again updates it and returns the previous value.
+        let v1 = store.set("t1", "hello".into(), "world1".into());
+        assert_eq!(v1, Ok(Some("world".into())));
+
+        // get The key that exists gets the latest value
+        let v = store.get("t1", "hello");
+        assert_eq!(v, Ok(Some("world1".into())));
+
+        // get The key that is not exists gets None
+        assert_eq!(Ok(None), store.get("t1", "hello1"));
+        assert!(store.get("t2", "hello1").unwrap().is_none());
+
+        // contains returns true if the key exists, otherwise false
+        assert_eq!(store.contains("t1", "hello"), Ok(true));
+        assert_eq!(store.contains("t1", "hello1"), Ok(false));
+        assert_eq!(store.contains("t2", "hello"), Ok(false));
+
+        // del the existing key to return the previous value
+        let v = store.del("t1", "hello");
+        assert_eq!(v, Ok(Some("world1".into())));
+
+        // del non-existent key or table Returns None
+        assert_eq!(Ok(None), store.del("t1", "hello1"));
+        assert_eq!(Ok(None), store.del("t2", "hello"));
+    }
+
+    fn test_get_all(store: impl Storage) {
+        store.set("t2", "k1".into(), "v1".into()).unwrap();
+        store.set("t2", "k2".into(), "v2".into()).unwrap();
+        let mut data = store.get_all("t2").unwrap();
+        data.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        assert_eq!(
+            data,
+            vec![
+                Kvpair::new("k1", "v1".into()),
+                Kvpair::new("k2", "v2".into())
+            ]
+        )
+    }
+
+    #[allow(dead_code)]
+    fn test_get_iter(store: impl Storage) {
+        store.set("t2", "k1".into(), "v1".into()).unwrap();
+        store.set("t2", "k2".into(), "v2".into()).unwrap();
+        let mut data: Vec<_> = store.get_iter("t2").unwrap().collect();
+        data.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        assert_eq!(
+            data,
+            vec![
+                Kvpair::new("k1", "v1".into()),
+                Kvpair::new("k2", "v2".into())
+            ]
+        )
+    }
+}
