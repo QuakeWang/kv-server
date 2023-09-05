9a0e72503619f6b4206b992c0f8e06435370155c
QuakeWang <1677980708@qq.com>
2023-09-06 00:10:34 +0800

    support command service

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,16 +1,41 @@
 pub mod abi;
 
-use abi::{command_request::RequestData, *};
+use std::result;
+
+use abi::{ command_request::RequestData, * };
+use http::StatusCode;
+
+use crate::KvError;
 
 impl CommandRequest {
-    /// create HSET command
+    /// Create HSET command
     pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
         Self {
-            request_data: Some(RequestData::Hset(Hset {
-                table: table.into(),
-                pair: Some(Kvpair::new(key, value)),
-            }
-            )),
+            request_data: Some(
+                RequestData::Hset(Hset {
+                    table: table.into(),
+                    pair: Some(Kvpair::new(key, value)),
+                })
+            ),
+        }
+    }
+
+    /// Create HGET command
+    pub fn new_hget(table: impl Into<String>, key: impl Into<String>) -> Self {
+        Self {
+            request_data: Some(
+                RequestData::Hget(Hget {
+                    table: table.into(),
+                    key: key.into(),
+                })
+            ),
+        }
+    }
+
+    /// Create HGETALL command
+    pub fn new_hgetall(table: impl Into<String>) -> Self {
+        Self {
+            request_data: Some(RequestData::Hgetall(Hgetall { table: table.into() })),
         }
     }
 }
@@ -40,5 +65,60 @@ impl From<&str> for Value {
         Self {
             value: Some(value::Value::String(s.into())),
         }
+    }
+}
+
+/// Transform i64 to Value
+impl From<i64> for Value {
+    fn from(value: i64) -> Self {
+        Self {
+            value: Some(value::Value::Integer(value)),
+        }
+    }
+}
+
+/// Transform value to CommandResponse
+impl From<Value> for CommandResponse {
+    fn from(value: Value) -> Self {
+        Self {
+            status: StatusCode::OK.as_u16() as _,
+            values: vec![value],
+            ..Default::default()
+        }
+    }
+}
+
+/// Transform Vec<Kvpair> to CommandResponse
+impl From<Vec<Kvpair>> for CommandResponse {
+    fn from(value: Vec<Kvpair>) -> Self {
+        Self {
+            status: StatusCode::OK.as_u16() as _,
+            pairs: value,
+            ..Default::default()
+        }
+    }
+}
+
+/// Tansform KvError to CommandResponse
+impl From<KvError> for CommandResponse {
+    fn from(e: KvError) -> Self {
+        let mut result = Self {
+            status: StatusCode::INTERNAL_SERVER_ERROR.as_u16() as _,
+            message: e.to_string(),
+            values: vec![],
+            pairs: vec![],
+        };
+
+        match e {
+            KvError::NotFound(_, _) => {
+                result.status = StatusCode::NOT_FOUND.as_u16() as _;
+            }
+            KvError::InvalidCommand(_) => {
+                result.status = StatusCode::BAD_REQUEST.as_u16() as _;
+            }
+            _ => {}
+        }
+
+        result
     }
 }
---
+++ src/service/mod.rs
@@ -0,0 +1,102 @@
+use crate::{
+    command_request::RequestData, CommandRequest, CommandResponse, KvError, MemTable, Storage,
+};
+use std::sync::Arc;
+use tracing::debug;
+
+mod command_service;
+
+/// The abstract of handl
+pub trait CommandService {
+    /// Handle Command, return Response
+    fn execute(self, store: &impl Storage) -> CommandResponse;
+}
+
+/// The struct of Service
+pub struct Service<Store = MemTable> {
+    inner: Arc<ServiceInner<Store>>,
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self { inner: Arc::clone(&self.inner) }
+    }
+}
+
+/// The struct of Service inner
+pub struct ServiceInner<Store> {
+    store: Store,
+}
+
+impl<Store: Storage> Service<Store> {
+    pub fn new(store: Store) -> Self {
+        Self {
+            inner: Arc::new(ServiceInner { store }),
+        }
+    }
+
+    pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
+        debug!("Got request: {:?}", cmd);
+        // TODO: send on_received event
+        let res = dispatch(cmd, &self.inner.store);
+        debug!("Executed reponse: {:?}", res);
+        // TODO: send on_executed event
+         res
+    }
+}
+
+pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
+    match cmd.request_data {
+        Some(RequestData::Hget(parma)) => parma.execute(store),
+        Some(RequestData::Hgetall(param)) => param.execute(store),
+        Some(RequestData::Hset(param)) => param.execute(store),
+        None => KvError::InvalidCommand("Request has no data".into()).into(),
+        _ => KvError::Internal("Not implemented".into()).into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+
+    use super::*;
+    use crate::{ MemTable, Value };
+
+    #[test]
+    fn service_should_work() {
+        let service = Service::new(MemTable::default());
+        let cloned = service.clone();
+
+        let handle = thread::spawn(move || {
+            let res = cloned.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+            assert_res_ok(res, &[Value::default()], &[]);
+        });
+        handle.join().unwrap();
+
+        let res = service.execute(CommandRequest::new_hget("t1", "k1"));
+        assert_res_ok(res, &["v1".into()], &[]);
+    }
+}
+
+
+#[cfg(test)]
+use crate::{Kvpair, Value};
+
+// Return the success test
+#[cfg(test)]
+pub fn assert_res_ok(mut res: CommandResponse, values: &[Value], pairs: &[Kvpair]) {
+    res.pairs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    assert_eq!(res.status, 200);
+    assert_eq!(res.message, "");
+    assert_eq!(res.values, values);
+    assert_eq!(res.pairs, pairs);
+}
+
+// Return the failure test
+#[cfg(test)]
+pub fn assert_res_error(res: CommandResponse, code: u32, msg: &str) {
+    assert_eq!(res.status, code);
+    assert!(res.message.contains(msg));
+    assert_eq!(res.values, &[]);
+    assert_eq!(res.pairs, &[]);
+}
