62f44f8d3d00b3dadfe5dde27ea236c2cbd0e21b
QuakeWang <1677980708@qq.com>
2023-09-22 23:42:47 +0800

    fix ci

--- examples/server.rs
+++ examples/server.rs
@@ -1,14 +1,14 @@
 use anyhow::Result;
 use async_prost::AsyncProstStream;
 use futures::prelude::*;
-use kv_server::{ CommandRequest, CommandResponse, MemTable, Service }; 
+use kv_server::{CommandRequest, CommandResponse, MemTable, Service, ServiceInner};
 use tokio::net::TcpListener;
 use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    let service: Service = Service::new(MemTable::new());
+    let service: Service = ServiceInner::new(MemTable::new()).into();
     let addr = "127.0.0.1:9527";
     let listener = TcpListener::bind(addr).await?;
     info!("Start listening on {}", addr);
